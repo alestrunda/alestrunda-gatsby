ec0c9bcd0d23a52de3655c43ada6e3ca2e263638
Aleš Trunda <alestrunda@gmail.com>
2019-07-22 17:49:40 +0200

    post work page

--- src/templates/work.js
+++ src/templates/work.js
@@ -1,22 +1,74 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { menuPage } from "../menuItems"
 
 const Work = ({ data }) => {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
-    <Layout>
+    <Layout menuItems={menuPage} activePath="/work/">
       <SEO title={frontmatter.title} />
-      <div className="container">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        {frontmatter.technologies && (
-          <p>Technologies: {frontmatter.technologies}</p>
-        )}
-        {frontmatter.client && <p>Client: {frontmatter.client}</p>}
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+      <div className="page-title">
+        <div className="container">
+          <h1 className="heading-page">{frontmatter.title}</h1>
+        </div>
+      </div>
+      <div className="section-light section-content section-content--mid section-content--top-small section-content--sm-bottom-small">
+        <div className="container">
+          <div className="grid grid--big">
+            <div className="grid__item grid__item--lg-span-7">
+              <Img
+                fluid={frontmatter.image.childImageSharp.fluid}
+                className="img-responsive"
+              />
+            </div>
+            <div className="grid__item grid__item--lg-span-5 grid__item--break-md-30">
+              <div dangerouslySetInnerHTML={{ __html: html }} />
+              <div class="m20"></div>
+              <h4 class="text-uppercase">Project details</h4>
+              <div class="m5"></div>
+              <strong class="text-black">Date:</strong> {frontmatter.date}
+              <br />
+              <div class="m5"></div>
+              {frontmatter.technologies && (
+                <>
+                  <strong class="text-black">Technologies:</strong>{" "}
+                  {frontmatter.technologies}
+                  <div class="m5"></div>
+                </>
+              )}
+              {frontmatter.client && (
+                <>
+                  <strong class="text-black">Client:</strong>{" "}
+                  {frontmatter.client}
+                  <div class="m5"></div>
+                </>
+              )}
+              {frontmatter.urlGithub && (
+                <>
+                  <strong class="text-black">Github:</strong>{" "}
+                  <a href={frontmatter.urlGithub}>{frontmatter.urlGithub}</a>
+                  <div class="m5"></div>
+                </>
+              )}
+              {frontmatter.urlLive && (
+                <>
+                  <strong class="text-black">Website:</strong>{" "}
+                  <a
+                    href={frontmatter.urlLive}
+                    rel={frontmatter.urlNoFollow ? "nofollow" : ""}
+                  >
+                    {frontmatter.urlLive}
+                  </a>
+                  <div class="m5"></div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   )
@@ -29,9 +81,19 @@ export const pageQuery = graphql`
       frontmatter {
         client
         date(formatString: "MMMM DD, YYYY")
+        image {
+          childImageSharp {
+            fluid(maxWidth: 600) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         path
         technologies
         title
+        urlGithub
+        urlLive
+        urlNoFollow
       }
     }
   }
