a6e6a5f33ab7ff4654500c8f37e5198398fa5ae6
Aleš Trunda <alestrunda@gmail.com>
2020-07-26 13:01:00 +0200

    code cleaning

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,70 +1,68 @@
 import React from "react"
 import TimelineItem from "./TimelineItem"
 
-const Timeline = () => {
-  const delayInit = 200
-  const delayStep = 100
+const DELAY_INIT = 200
+const DELAY_STEP = 100
 
-  return (
-    <div className="timeline">
-      <TimelineItem delay={delayInit} from={2006} title="First experience">
-        First experience with web development in a school project.
-      </TimelineItem>
-      <TimelineItem
-        delay={delayInit + delayStep}
-        from={2012}
-        reverse
-        title="First part-time job"
-      >
-        First part-time job as WordPress developer at Nexdesign web studio.
-      </TimelineItem>
-      <TimelineItem
-        delay={delayInit + delayStep * 2}
-        from={2014}
-        title="Self-employed"
-      >
-        Full-time freelancer - mainly front-end and WordPress developer. Working
-        for end clients and dozens of web studios over the years.
-      </TimelineItem>
-      <TimelineItem
-        delay={delayInit + delayStep * 3}
-        from={2014}
-        reverse
-        to={2015}
-        title="ThemeForest developer"
-      >
-        Cooperating with other freelancers to develop templates and sell them on
-        ThemeForest.
-      </TimelineItem>
-      <TimelineItem
-        delay={delayInit + delayStep * 4}
-        from={2017}
-        title="React.js"
-      >
-        First project using modern JavaScript - web app build on React.
-      </TimelineItem>
-      <TimelineItem
-        delay={delayInit + delayStep * 5}
-        from={2019}
-        reverse
-        title="IT university"
-      >
-        BSc degree, thesis: educational web and mobile app in React and React
-        Native.
-      </TimelineItem>
-      <TimelineItem delay={delayInit + delayStep * 6} from={2019} title="QCM">
-        Full-time job as front-end developer.
-      </TimelineItem>
-      <TimelineItem
-        delay={delayInit + delayStep * 6}
-        from={2020}
-        reverse
-        title="Solar Winds"
-      >
-        Full-time job as a front-end developer in an international company.
-      </TimelineItem>
-    </div>
-  )
-}
+const Timeline = () => (
+  <div className="timeline">
+    <TimelineItem delay={DELAY_INIT} from={2006} title="First experience">
+      First experience with web development in a school project.
+    </TimelineItem>
+    <TimelineItem
+      delay={DELAY_INIT + DELAY_STEP}
+      from={2012}
+      reverse
+      title="First part-time job"
+    >
+      First part-time job as WordPress developer at Nexdesign web studio.
+    </TimelineItem>
+    <TimelineItem
+      delay={DELAY_INIT + DELAY_STEP * 2}
+      from={2014}
+      title="Self-employed"
+    >
+      Full-time freelancer - mainly front-end and WordPress developer. Working
+      for end clients and dozens of web studios over the years.
+    </TimelineItem>
+    <TimelineItem
+      delay={DELAY_INIT + DELAY_STEP * 3}
+      from={2014}
+      reverse
+      to={2015}
+      title="ThemeForest developer"
+    >
+      Cooperating with other freelancers to develop templates and sell them on
+      ThemeForest.
+    </TimelineItem>
+    <TimelineItem
+      delay={DELAY_INIT + DELAY_STEP * 4}
+      from={2017}
+      title="React.js"
+    >
+      First project using modern JavaScript - web app build on React.
+    </TimelineItem>
+    <TimelineItem
+      delay={DELAY_INIT + DELAY_STEP * 5}
+      from={2019}
+      reverse
+      title="IT university"
+    >
+      BSc degree, thesis: educational web and mobile app in React and React
+      Native.
+    </TimelineItem>
+    <TimelineItem delay={DELAY_INIT + DELAY_STEP * 6} from={2019} title="QCM">
+      Full-time job as front-end developer.
+    </TimelineItem>
+    <TimelineItem
+      delay={DELAY_INIT + DELAY_STEP * 6}
+      from={2020}
+      reverse
+      title="Solar Winds"
+    >
+      Full-time job as a front-end developer in an international company.
+    </TimelineItem>
+  </div>
+)
 
 export default Timeline
