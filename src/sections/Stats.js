996d8b5f42b182ff7a12f2f7a8e8e9bc472f7ea8
Aleš Trunda <alestrunda@gmail.com>
2021-03-19 22:10:09 +0100

    update Stats

--- src/sections/Stats.js
+++ src/sections/Stats.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import Img from "gatsby-image"
 import { StaticQuery, graphql } from "gatsby"
 import { PROFESSIONAL_CAREER_START_YEAR } from "../constants"
@@ -8,75 +8,95 @@ import InViewAnim from "../components/InViewAnim"
 const getYearsOfExperience = () =>
   new Date().getFullYear() - PROFESSIONAL_CAREER_START_YEAR
 
-const Stats = ({ bgImage }) => (
-  <section id="stats" className="bg-wrapper">
-    <Img fluid={bgImage} className="bg-img" alt="" />
-    <div className="section-dark section-content">
-      <div className="container text-center">
-        <header className="heading-box">
-          <div className="decoration-border decoration-border--top decoration-border--tiny"></div>
-          <div className="decoration-border decoration-border--bottom decoration-border--tiny"></div>
-          <h1 className="heading-box__title">Some interesting facts</h1>
-        </header>
-        <div className="grid grid--clean box-info-grid">
-          <InViewAnim
-            className="grid__item grid__item--xl-span-3 grid__item--lg-span-6"
-            delay="100"
-          >
-            {(isAnimated) => (
-              <CounterInfo
-                iconClassName="fa fa-smile"
-                text="satisfied clients"
-                value={64}
-                started={isAnimated}
-                duration={6}
-              />
-            )}
-          </InViewAnim>
-          <InViewAnim
-            className="grid__item grid__item--xl-span-3 grid__item--lg-span-6 grid__item--break-md-50"
-            delay="200"
-          >
-            {(isAnimated) => (
-              <CounterInfo
-                iconClassName="fa fa-thumbs-down"
-                text="sloppy projects"
-                value={0}
-                started={isAnimated}
-              />
-            )}
-          </InViewAnim>
-          <InViewAnim
-            className="grid__item grid__item--xl-span-3 grid__item--lg-span-6 grid__item--break-lg-50"
-            delay="300"
-          >
-            {(isAnimated) => (
-              <CounterInfo
-                iconClassName="fa fa-chart-line"
-                text="years of experience"
-                value={getYearsOfExperience()}
-                started={isAnimated}
-              />
-            )}
-          </InViewAnim>
-          <InViewAnim
-            className="grid__item grid__item--xl-span-3 grid__item--lg-span-6 grid__item--break-lg-50"
-            delay="400"
-          >
-            {(isAnimated) => (
-              <CounterInfo
-                iconClassName="fa fa-graduation-cap"
-                text="hours per week self-studying"
-                value={8}
-                started={isAnimated}
-              />
-            )}
-          </InViewAnim>
+const Stats = ({ bgImage }) => {
+  const [githubStats, setGithubStats] = useState({})
+  const isUnmounted = useRef()
+
+  useEffect(() => {
+    fetch("https://iq22l0wv25.execute-api.us-east-1.amazonaws.com/stats")
+      .then((response) => response.json())
+      .then((response) => {
+        if (isUnmounted.current) return
+        setGithubStats(response)
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+    return () => {
+      isUnmounted.current = true
+    }
+  })
+
+  return (
+    <section id="stats" className="bg-wrapper">
+      <Img fluid={bgImage} className="bg-img" alt="" />
+      <div className="section-dark section-content">
+        <div className="container text-center">
+          <header className="heading-box">
+            <div className="decoration-border decoration-border--top decoration-border--tiny"></div>
+            <div className="decoration-border decoration-border--bottom decoration-border--tiny"></div>
+            <h1 className="heading-box__title">Some interesting facts</h1>
+          </header>
+          <div className="grid grid--clean box-info-grid">
+            <InViewAnim
+              className="grid__item grid__item--xl-span-3 grid__item--lg-span-6"
+              delay="100"
+            >
+              {(isAnimated) => (
+                <CounterInfo
+                  iconClassName="fab fa-github"
+                  text="my recent github commits"
+                  value={githubStats.commits_cnt || 0}
+                  started={isAnimated}
+                  duration={6}
+                />
+              )}
+            </InViewAnim>
+            <InViewAnim
+              className="grid__item grid__item--xl-span-3 grid__item--lg-span-6 grid__item--break-md-50"
+              delay="200"
+            >
+              {(isAnimated) => (
+                <CounterInfo
+                  iconClassName="fa fa-thumbs-down"
+                  text="sloppy projects"
+                  value={0}
+                  started={isAnimated}
+                />
+              )}
+            </InViewAnim>
+            <InViewAnim
+              className="grid__item grid__item--xl-span-3 grid__item--lg-span-6 grid__item--break-lg-50"
+              delay="300"
+            >
+              {(isAnimated) => (
+                <CounterInfo
+                  iconClassName="fa fa-chart-line"
+                  text="years of professional experience"
+                  value={getYearsOfExperience()}
+                  started={isAnimated}
+                />
+              )}
+            </InViewAnim>
+            <InViewAnim
+              className="grid__item grid__item--xl-span-3 grid__item--lg-span-6 grid__item--break-lg-50"
+              delay="400"
+            >
+              {(isAnimated) => (
+                <CounterInfo
+                  iconClassName="fa fa-graduation-cap"
+                  text="hours per week studying"
+                  value={8}
+                  started={isAnimated}
+                />
+              )}
+            </InViewAnim>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 const statsWrapper = () => (
   <StaticQuery
