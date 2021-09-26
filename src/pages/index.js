f8eee1e24e24aec8aa654408c35b20e24d21fdec
Aleš Trunda <alestrunda@gmail.com>
2021-09-26 20:25:17 +0200

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => (
     headerClassName="page-header--fixed"
     menuItems={menuIndex}
   >
-    <PageSeo title="Home" />
+    <PageSeo title="Frontend developer, react" />
     <MainSlider />
     <About />
     <Citation />
